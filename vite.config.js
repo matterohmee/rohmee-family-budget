4b7429fa140893dceae70f384c6c56f0f117a693
matterohmee <matterohmee@hotmail.com>
2025-08-11 21:21:43 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: '.',                       // project root is the repo root
+  publicDir: false,                // we’re not using /public for static HTML
   server: { host: true },
-  build: { outDir: 'dist', sourcemap: false },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'          // <-- force Vite/Rollup to this entry
+    }
+  }
 })
